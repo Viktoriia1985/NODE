6bc438ea43f74ccccff5a43b0d90ae7c6d3aac4c
Viktoriia Krat <flightsspb@gmail.com>
2021-09-07 16:51:13 +0300

    initial

---
+++ homework_2/db/users.js
@@ -0,0 +1,6 @@
+module.exports = [
+    {name: 'Dima', age: 36, email: 'Dimon_2000@gmail.com', password: 'Pa$$w0rd'},
+    {name: 'Vika', age: 25,  email: 'Vika23@gmail.com', password: 'Pa$$w0rd'},
+    {name: 'Olga', age: 26,  email: 'Goodgirl@gmail.com', password: 'Pa$$w0rd'},
+    {name: 'Viktor', age: 26,  email: 'Young_eagle@gmail.com', password: 'Pa$$w0rd'},
+]

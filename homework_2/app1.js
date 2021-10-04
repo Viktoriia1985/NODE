f4a81ab7022fe8e4a01cc23c0e9f0175a2b8f021
Viktoriia Krat <flightsspb@gmail.com>
2021-10-04 18:52:39 +0300

    LESSON_2

--- homework_2/app1.js
+++ homework_2/app1.js
@@ -11,12 +11,15 @@
 const express = require('express');
 const expressHbs = require('express-handlebars');
 const path = require('path');
+const { promisify } = require('util');
 
-const { PORT } = require('./config/variables');
+const { variables: { PORT } } = require('./config');
 
 const app1 = express();
 
 const fs = require('fs');
+const readPromisify = promisify(fs.readFile);
+const writePromisify = promisify(fs.writeFile);
 
 app1.use(express.json());
 app1.use(express.urlencoded({ extended: true }));
@@ -30,9 +33,17 @@ app1.get('/', (req, res) => {
     res.render('homeTask')
 });
 
+const readFile = async () => {
+    const data = await readPromisify(path.join(__dirname, 'db', 'users.json'));
+    return JSON.parse(data.toString());
+}
+
+const writeFile = (arr) => {
+    return writePromisify(path.join(__dirname, 'db', 'users.json'), JSON.stringify(arr));
+}
+
 app1.get('/users', async (req, res) => {
-    const data = await fs.readFileSync(path.join(__dirname, 'db', 'users.json'))
-    const users = JSON.parse(data.toString());
+       const users = await readFile();
 
     res.render('users', { users });
 })
@@ -40,13 +51,12 @@ app1.get('/users', async (req, res) => {
 app1.post('/users', async (req, res) => {
     const { email, password } = req.body;
 
-    const data = await fs.readFileSync(path.join(__dirname, 'db', 'users.json'))
-    const users = JSON.parse(data.toString());
+    const users = await readFile();
 
     const user = users.find(user => user.email === email && user.password === password);
 
     if (!user) {
-        res.redirect('/registration')
+        res.redirect('/registration');
         return;
     }
 
@@ -57,8 +67,7 @@ app1.post('/users', async (req, res) => {
 app1.get('/users/:user_id', async (req, res) => {
     const { user_id } = req.params;
 
-    const data = await fs.readFileSync(path.join(__dirname, 'db', 'users.json'))
-    const users = JSON.parse(data.toString());
+    const users = await readFile();
 
     const currentUser = users[user_id];
 
@@ -70,14 +79,13 @@ app1.get('/users/:user_id', async (req, res) => {
 });
 
 app1.get('/login', (req, res) => {
-    res.render('login')
-})
+    res.render('login');
+});
 
 app1.post('/login', async (req, res) => {
     const { email, password } = req.body;
 
-    const data = await fs.readFileSync(path.join(__dirname, 'db', 'users.json'));
-    const users = JSON.parse(data.toString());
+    const users  = await readFile();
 
     const user = users.find(user => user.email === email && user.password === password);
 
@@ -85,6 +93,7 @@ app1.post('/login', async (req, res) => {
         res.redirect('/registration');
         return;
     }
+
     res.redirect('/users');
     res.end();
 });
@@ -97,13 +106,12 @@ app1.get('/registration', (req, res) => {
 app1.post('/registration', async (req, res) => {
     const { body, body: { email, password, name, age } } = req;
 
-    const data = await fs.readFileSync(path.join(__dirname, 'db', 'users.json'))
-    const users = JSON.parse(data.toString());
-
     if (!email || !password || !name || !age) {
         res.status(400).end('BAD REQUEST');
         return;
     }
+
+    const users = await readFile();
 
     const userForRegister = users.find(user => user.email === body.email);
 
@@ -113,14 +121,13 @@ app1.post('/registration', async (req, res) => {
     }
 
     users.push(body);
-    await fs.writeFileSync(path.join(__dirname, 'db', 'users.json'), JSON.stringify(users))
+    await writeFile(users);
 
     res.redirect('/login');
     res.end();
 });
 
-
 app1.listen(PORT, () => {
     console.log('App listen', PORT);
-})
+});
 

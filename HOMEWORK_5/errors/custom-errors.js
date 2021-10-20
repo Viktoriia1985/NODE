810f8640bb999aaa0422f6eddd1c31720125b429
feden2906 <feden2906@gmail.com>
2021-10-20 15:30:21 +0300

    fix

--- HOMEWORK_5/errors/custom-errors.js
+++ HOMEWORK_5/errors/custom-errors.js
@@ -1,7 +1,7 @@
 module.exports = {
     EMAIL_REGISTERED: {
         message: 'Email already exists',
-        code: 400
+        code: 409
     },
 
     NOT_VALID_BODY: {
@@ -12,11 +12,6 @@ module.exports = {
     FORBIDDEN_REQUEST: {
         message: 'Access denied',
         code: 403
-    },
-
-    NOT_FOUND: {
-        message: 'User not found',
-        code: 404
     },
 
     NOT_FOUND_BY_ID: {
